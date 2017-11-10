cee6067acab1438e8ec57c23dcc6b645e6522fa0
AnnaDiazPineda <31926227+AnnaDiazPineda@users.noreply.github.com>
2017-11-10 15:13:32 +0100

    ++Test Jugador

--- rest-example/src/main/java/edu/upc/dsa/Jugador.java
+++ rest-example/src/main/java/edu/upc/dsa/Jugador.java
@@ -5,4 +5,13 @@ public class Jugador {
     private String email;
     private String contrasenya;
     private Usuario personantge;
+
+    public Jugador(String nom, String contrasenya, String email, Usuario personantge)
+    {
+        this.nom = nom;
+        this.contrasenya = contrasenya;
+        this.email = email;
+        this.personantge = personantge;
+
+    }
 }
