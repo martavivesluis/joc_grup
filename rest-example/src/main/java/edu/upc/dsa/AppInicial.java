98b2391d0dfed4b77ae4c0344b63dc893aa1412b
AnnaDiazPineda <31926227+AnnaDiazPineda@users.noreply.github.com>
2017-11-11 05:57:46 +0100

    ++Test service of Json

--- rest-example/src/main/java/edu/upc/dsa/AppInicial.java
+++ rest-example/src/main/java/edu/upc/dsa/AppInicial.java
@@ -6,7 +6,25 @@ import java.util.Scanner;
 public class AppInicial {
     SingletonMundo instanciaunica = SingletonMundo.getInstance();
 
+    Scanner sn = new Scanner(System.in);
+    int opcio;
+    boolean sortida = false;
+    while (!sortida)
 
+    {
+        System.out.println("Escull una de les opcions");
+        System.out.println("-------------------------");
+        System.out.println("1. Loggin");
+        System.out.println("2. Registar-se");
+        System.out.println("3. Instruccions Joc");
+        System.out.println("4. Sortir");
+
+
+        opcio = sn.nextInt();
+
+    }
 
 
 }
+
+
