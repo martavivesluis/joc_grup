3cb0f566966490e3595c1b9fbbdefaf95dfb3d67
AnnaDiazPineda <31926227+AnnaDiazPineda@users.noreply.github.com>
2017-11-15 15:52:09 +0100

    ++ juagador rest

--- rest-example/src/main/java/edu/upc/dsa/JSONService.java
+++ rest-example/src/main/java/edu/upc/dsa/JSONService.java
@@ -1,75 +1,45 @@
 package edu.upc.dsa;
 
 import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 @Path("/json")
 public class JSONService {
-    private Mundo mundo =  SingletonMundo.getInstance().mundo;
+    public JSONService() {
 
-    public JSONService() { }
-    @POST
-    @Path("/usuario/new")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response newTrack(Usuario user) {
-       //mundo.crearUsuario(user);
-       return Response.status(201).entity("User added!").build();
+
+     Mundo mundo = SingletonMundo.getInstance().mundo;
+    //creació d'un jugador
+    Jugador j1 = new Jugador();
+    j1.setNom("anna");
+    j1.setContrasenya("anna");
+
     }
-
     @GET
-    @Path("/usuario/{nombre}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Usuario getTrack(@PathParam("nombre") String nombre) {
-        return mundo.consultarUsuario(nombre);
+    @Path("/got/{nom}")//variable entra pels corxets
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt()
+    {
+        return "Got it";
     }
-/*
+    @Path("jugadors/{jugadorName}")
     @GET
-    @Path("/all")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Track> getAllTracks() {
-        ArrayList<Track> tracks = SingletonMundo.getInstance().array;
-        return tracks;
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getUser(@PathParam("jugadorName") String jugadorName) {
+        return "Benvingut " + jugadorName;
+
     }
 
-
+    @Path("exception")
     @GET
-    @Path("/get")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Track getTrackInJSON() {
-
-        Track track = new Track();
-        track.setTitle("Enter Sandman");
-        track.setSinger("Metallica");
-
-        return track;
-
+    @Produces(MediaType.TEXT_PLAIN)
+    public String throwIt() throws Exception
+    {
+        throw new Exception("My Exception");
     }
-
-
-    @POST
-    @Path("/delete")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response deleteTrack(Track track) {
-        ArrayList<Track> tracks = SingletonMundo.getInstance().array;
-        tracks.remove(track);
-        // Atencion: siempre añade en la misma posicion por el scope de tracks
-        return Response.status(201).entity("Track added in position "+tracks.size()).build();
-    }
-
-    @POST
-    @Path("/post")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response createTrackInJSON(Track track) {
-
-        ArrayList<Track> tracks = SingletonMundo.getInstance().array;;
-        String result = "Track saved in array : " + track;
-        tracks.add(track);
-        return Response.status(201).entity(result).build();
-    }
-*/
 
 }
