ad0c6375ad23c9081cf0abdda1de65b18c4b93a4
Nodet <noodet@gmail.com>
2017-12-02 19:35:18 +0100

    Merge de la API Funcional (Ver. Final)

--- rest-example/src/main/java/edu/upc/dsa/JSONService.java
+++ rest-example/src/main/java/edu/upc/dsa/JSONService.java
@@ -7,13 +7,114 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("json")//porta
+@Path("/")
 public class JSONService {
     BDTemporal BDtemp;
-    SingletonMundo mimundo;
 
     public JSONService() {
-        mimundo = SingletonMundo.getInstance();
+        BDtemp = BDTemporal.getDbInstancia();
+    }
+
+    @GET
+    @Path("/Jugador/{nombre}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Jugador buscarJugador(@PathParam("nombre") String nombre) {
+        return BDtemp.getMapaJugadores().get(nombre);
+    }
+
+    @GET
+    @Path("/Personaje/{nombre}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Personatge buscarPersonaje(@PathParam("nombre") String nombre) {
+        return BDtemp.getMapaPersonajes().get(nombre);
+    }
+
+    @GET
+    @Path("/Objeto/{nombre}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Objeto buscarObjeto(@PathParam("nombre") String nombre) {
+        return BDtemp.getMapaObjetos().get(nombre);
+    }
+
+
+    @POST
+    @Path("/nuevoJugador/{jugadorNombre}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response nuevoJugador(Jugador jugador, @PathParam("jugadorNombre") String nombre) {
+        jugador.setNom(nombre);
+        BDtemp.getMapaJugadores().put(nombre, jugador);
+        return Response.status(201).entity("Añadido jugador con nombre:" +nombre).build();
+    }
+
+    @POST
+    @Path("/eliminarJugador/{jugadorNombre}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response eliminarJugador(@PathParam("jugadorNombre") String nombre) {
+        BDtemp.getMapaJugadores().remove(nombre);
+        return Response.status(201).entity("Eliminado jugador con nombre:" +nombre).build();
+    }
+
+    @POST
+    @Path("/nuevoPersonaje/{personajeNombre}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response nuevoPersonaje(Personatge personaje, @PathParam("personajeNombre") String nombre) {
+        personaje.setNombre(nombre);
+        BDtemp.getMapaPersonajes().put(nombre, personaje);
+        return Response.status(201).entity("Añadido personaje con nombre:" +nombre).build();
+    }
+
+    @POST
+    @Path("/eliminarPersonaje/{personajeNombre}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response eliminarPersonaje(@PathParam("personajeNombre") String nombre) {
+        BDtemp.getMapaPersonajes().remove(nombre);
+        return Response.status(201).entity("Eliminado personaje con nombre:" +nombre).build();
+    }
+
+    @POST
+    @Path("/nuevoObjeto/{objetoNombre}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response nuevoObjeto(Objeto objeto, @PathParam("objetoNombre") String nombre) {
+        objeto.setNombre(nombre);
+        BDtemp.getMapaObjetos().put(nombre, objeto);
+        return Response.status(201).entity("Añadido objeto con nombre:" +nombre).build();
+    }
+
+    @POST
+    @Path("/eliminarObjeto/{objetoNombre}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response eliminarObjeto(@PathParam("objetoNombre") String nombre) {
+        BDtemp.getMapaObjetos().remove(nombre);
+        return Response.status(201).entity("Eliminado objeto con nombre:" +nombre).build();
+    }
+
+    @POST
+    @Path("/nuevoObjetoPersonaje/{personajeNombre}/{objetoNombre}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response nuevoObjetoPersonaje(@PathParam("personajeNombre") String nombrePersonaje,
+                                         @PathParam("objetoNombre") String nombreObjeto) {
+
+        //Añadir comprobaciones de errores y tal
+        BDtemp.getMapaPersonajes().get(nombrePersonaje).getArrMisObjetos().add(BDtemp.getMapaObjetos().get(nombreObjeto));
+
+        return Response.status(201).entity("Añadido objeto con nombre:" +nombreObjeto +"a personaje" +nombrePersonaje).build();
+    }
+
+    @POST
+    @Path("/nuevoPersonajeJugador/{jugadorNombre}/{personajeNombre}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response nuevoPersonajeJugador(@PathParam("jugadorNombre") String jugadorNombre,
+                                          @PathParam("personajeNombre") String personajeNombre) {
+
+        //Añadir comprobaciones de errores y tal
+        BDtemp.getMapaJugadores().get(jugadorNombre).getPersonatges().add(BDtemp.getMapaPersonajes().get(personajeNombre));
+
+        return Response.status(201).entity("Añadido personaje con nombre:" +personajeNombre +"a jugador" +jugadorNombre).build();
+    }
+
+
+
+    /*   mimundo = SingletonMundo.getInstance();
         Jugador j = new Jugador("marta","1234","martavivesluis@gmail.com");
         mimundo.mundo.jugadores.put(j.getId(),j);
        Personatge p =  new Personatge("Hamlet",1,1,1,1);
@@ -38,12 +139,6 @@ public class JSONService {
         return "false";
     }
 
-    @GET
-    @Path("/Personaje/{nombre}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Personatge buscarPersonaje(@PathParam("nombre") String nombre) {
-        return new Personatge("Hamlet",1,1,1,1);
-    }
 
     @GET
     @Path("/Mapa")
@@ -53,112 +148,6 @@ public class JSONService {
         ObjectMapper mapper = new ObjectMapper();
         mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         return mapper.writeValueAsString( mimundo.mundo.mapa);
-    }
-    /*
-
-
-    @GET
-    @Path("/Jugador/{nombre}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Jugador buscarJugador(@PathParam("nombre") String nombre) {
-        return jugadores.getMapaJugadores().get(nombre);
-    }
-
-
-
-    @GET
-    @Path("/Objeto/{nombre}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Objeto buscarObjeto(@PathParam("nombre") String nombre) {
-        return jugadores.getMapaObjetos().get(nombre);
-    }
-
-
-    @POST
-    @Path("/nuevoJugador/{jugadorNombre}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response nuevoJugador(Jugador jugador, @PathParam("jugadorNombre") String nombre) {
-        jugador.setNombre(nombre);
-        jugadores.getMapaJugadores().put(nombre, jugador);
-        return Response.status(201).entity("Añadido jugador con nombre:" +nombre).build();
-    }
-
-    @POST
-    @Path("/eliminarJugador/{jugadorNombre}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response eliminarJugador(@PathParam("jugadorNombre") String nombre) {
-        jugadores.getMapaJugadores().remove(nombre);
-        return Response.status(201).entity("Eliminado jugador con nombre:" +nombre).build();
-    }
-
-    @POST
-    @Path("/nuevoPersonaje/{personajeNombre}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response nuevoPersonaje(Personatge personaje, @PathParam("personajeNombre") String nombre) {
-        personaje.setNombre(nombre);
-        jugadores.getMapaPersonajes().put(nombre, personaje);
-        return Response.status(201).entity("Añadido personaje con nombre:" +nombre).build();
-    }
-
-    @POST
-    @Path("/eliminarPersonaje/{personajeNombre}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response eliminarPersonaje(@PathParam("personajeNombre") String nombre) {
-        jugadores.getMapaPersonajes().remove(nombre);
-        return Response.status(201).entity("Eliminado personaje con nombre:" +nombre).build();
-    }
-
-    @POST
-    @Path("/nuevoObjeto/{objetoNombre}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response nuevoObjeto(Objeto objeto, @PathParam("objetoNombre") String nombre) {
-        objeto.setNombre(nombre);
-        jugadores.getMapaObjetos().put(nombre, objeto);
-        return Response.status(201).entity("Añadido objeto con nombre:" +nombre).build();
-    }
-
-    @POST
-    @Path("/eliminarObjeto/{objetoNombre}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response eliminarObjeto(@PathParam("objetoNombre") String nombre) {
-        jugadores.getMapaObjetos().remove(nombre);
-        return Response.status(201).entity("Eliminado objeto con nombre:" +nombre).build();
-    }
-
-    @POST
-    @Path("/nuevoObjetoPersonaje/{personajeNombre}/{objetoNombre}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response nuevoObjetoPersonaje(@PathParam("personajeNombre") String nombrePersonaje,
-                                         @PathParam("objetoNombre") String nombreObjeto) {
-
-        //Añadir comprobaciones de errores y tal
-        jugadores.getMapaPersonajes().get(nombrePersonaje).getObjetos().add(jugadores.getMapaObjetos().get(nombreObjeto));
-
-        return Response.status(201).entity("Añadido objeto con nombre:" +nombreObjeto +"a personaje" +nombrePersonaje).build();
-    }
-
-    @POST
-    @Path("/nuevoPersonajeJugador/{jugadorNombre}/{personajeNombre}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response nuevoPersonajeJugador(@PathParam("jugadorNombre") String jugadorNombre,
-                                          @PathParam("personajeNombre") String personajeNombre) {
-
-        //Añadir comprobaciones de errores y tal
-        jugadores.getMapaJugadores().get(jugadorNombre).getPersonajes().add(jugadores.getMapaPersonajes().get(personajeNombre));
-
-        return Response.status(201).entity("Añadido personaje con nombre:" +personajeNombre +"a jugador" +jugadorNombre).build();
-    }
-
-
-
-
-    @Path("exception")
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String throwIt() throws Exception
-    {
-        throw new Exception("My Exception");
-    }
-    */
+    }*/
 
 }
